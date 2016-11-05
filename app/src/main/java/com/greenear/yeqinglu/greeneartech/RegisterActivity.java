f6ebbea4e748e63189e456fcf0554043c940bdab
Yeqing <yeqing15@gmail.com>
2016-11-05 11:30:47 +0800

    add RegisterActivity

---
+++ app/src/main/java/com/greenear/yeqinglu/greeneartech/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.greenear.yeqinglu.greeneartech;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yeqing.lu on 2016/11/5.
+ */
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
